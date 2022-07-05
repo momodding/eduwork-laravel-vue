ffcd3acc82071e8526715811205b410e19dc5909
andreleonardojudah <andre.leonardo.judah@gmail.com>
2022-07-05 11:52:11 +0700

    (PHP dan MySQL) Connect Database

---
+++ Tugas Connect Database Perpustakaan 002.php
@@ -0,0 +1,32 @@
+<?php
+$servername = "localhost";
+$database = "perpus";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
+if (!$conn) {
+	die("Connection failed: " .$mysqli->connect_error());
+ 	}
+
+// echo "Connected successfully";
+// mysqli_close($conn);
+
+$sql = "SELECT nama, alamat FROM anggota WHERE alamat LIKE '%Bandung%'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+	// output data of each row
+	while ($row = $result->fetch_assoc()) {
+		echo "anggota : " . $row["nama"]. " " . $row["alamat"]. "<br>";
+	}
+} else {
+  echo "0 results";
+}
+$conn->close();
+
+
+?>
