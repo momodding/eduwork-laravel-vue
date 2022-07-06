c482d771d5e285498c970d04921ffcc963bde064
andreleonardojudah <andre.leonardo.judah@gmail.com>
2022-07-06 19:11:39 +0700

    (Laravel x Vue.js) Library

--- Laravel x Vue.js Library/app/Http/Controllers/HomeController.php
+++ Laravel x Vue.js Library/app/Http/Controllers/HomeController.php
@@ -35,7 +35,7 @@ class HomeController extends Controller
         // $publishers = Publisher::with('books')->get();
         
 
-        return $catalogs;
+        // return $catalogs;
         return view('home');
     }
 }
