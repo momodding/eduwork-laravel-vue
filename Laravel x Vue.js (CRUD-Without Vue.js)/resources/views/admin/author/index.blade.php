27f91e1576146b76a18595b07fe336f9da96d7b4
andreleonardojudah <andre.leonardo.judah@gmail.com>
2022-07-24 18:26:57 +0700

    (Laravel x Vue.js) CRUD-Without Vue.js

---
+++ Laravel x Vue.js (CRUD-Without Vue.js)/resources/views/admin/author/index.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.admin')
+@section('header', 'Author')
+
+@section('content')
+<div class="row">
+    <div class="col-md-6">
+      <div class="card"></div>
+              <div class="card-header">
+                <a href="{{ url('authors/create') }}" class="btn btn-sm btn-primary pull-right">Create New Author</a>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <table class="table table-bordered">
+                  <thead>
+                    <tr>
+                      <th style="width: 10px" class="text-center">No.</th>
+                      <th class="text-center">Name</th>
+                      <th class="text-center">Email</th>
+                      <th class="text-center">Phone Number</th>
+                      <th class="text-center">Address</th>
+                      <th class="text-center">Created At</th>
+                      <th class="text-center">Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    @foreach($authors as $key => $author)
+                    <tr>
+                      <td class="text-center">{{ $key+1 }}</td>
+                      <td class="text-center">{{ $author->name }}</td>
+                      <td class="text-center">{{ $author->email }}</td>
+                      <td class="text-center">{{ $author->phone_number }}</td>
+                      <td class="text-center">{{ $author->address }}</td>
+                      <td class="text-center">{{ date('H:i:s | d/M/Y', strtotime($author->created_at)) }}</td>
+                      <td class="text-center">
+                        <a href="{{ url('authors/'.$author->id.'/edit') }}" class="btn btn-warning btn-sm" style="width: 100px">Edit</a> <br><br>
+                      
+                      <form action="{{ url('authors', ['id' => $author->id]) }}" method="post">
+                          <input class="btn btn-danger btn-sm" style="width: 100px" type="submit" value="Delete" onclick="return confirm('Are you sure?')">
+                          @method('delete')
+                          @csrf
+                      </form>
+                      </td>
+                    </tr>
+                    @endforeach
+                  </tbody>
+                </table>
+              </div>
+
+            <!-- <div class="card-footer clearfix">
+                <ul class="pagination pagination-sm m-0 float-right">
+                  <li class="page-item"><a class="page-link" href="#">&laquo;</a></li>
+                  <li class="page-item"><a class="page-link" href="#">1</a></li>
+                  <li class="page-item"><a class="page-link" href="#">2</a></li>
+                  <li class="page-item"><a class="page-link" href="#">3</a></li>
+                  <li class="page-item"><a class="page-link" href="#">&raquo;</a></li>
+                </ul>
+              </div> -->
+            </div>
+          </div>
+        </div>
+@endsection
