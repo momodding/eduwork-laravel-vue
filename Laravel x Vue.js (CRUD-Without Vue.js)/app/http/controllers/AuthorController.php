49324d890a8943be60910a625eb6ed18c7512152
andreleonardojudah <andre.leonardo.judah@gmail.com>
2022-07-24 18:30:27 +0700

    (Laravel x Vue.js) CRUD-Without Vue.js

---
+++ Laravel x Vue.js (CRUD-Without Vue.js)/app/http/controllers/AuthorController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Author;
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $authors = Author::with('books')->get();
+
+        //return $authors;
+        return view('admin.author.index', compact('authors'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.author.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+        {
+        // Security validasi backend untuk validasi input data catalog untuk function create
+
+        $this->validate($request,[
+            'name', 'email', 'phone_number', 'address'      =>['required', 'string', 'min:5', 'max:60'],
+        ]);
+
+        // Cara pertama untuk memasukkan data ke table author
+
+        // $author = new Author;
+        // $author->name = $request->name;
+        // $author->save();
+
+        // Cara kedua untuk memasukkan data ke table author dan masukkan protected $fillable = ['']; ke models author.php
+
+        Catalog::create($request->all());
+
+        return redirect('authors');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Author  $author
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Author $author)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Author  $author
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Author $author)
+    {
+        return view('admin.author.edit', compact('author'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Author  $author
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Author $author)
+    {
+        $this->validate($request,[
+            'name'      =>['required'],
+        ]);
+
+        $author->update($request->all());
+
+        return redirect('authors');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Author  $author
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Author $author)
+    {
+        
+        $author->delete();
+
+        return redirect('authors');
+    }
+}
