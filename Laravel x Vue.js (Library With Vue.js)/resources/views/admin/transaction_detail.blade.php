ee0c66e3abca572fa65c91e6434e691cf6dcf8a0
andreleonardojudah <andre.leonardo.judah@gmail.com>
2022-07-29 16:08:47 +0700

    (Laravel x Vue.js) Library With Vue.js

--- Laravel x Vue.js (Library With Vue.js)/resources/views/admin/transaction_detail.blade.php
+++ Laravel x Vue.js (Library With Vue.js)/resources/views/admin/transaction_detail.blade.php
@@ -2,7 +2,10 @@
 @section('header', 'Transaction Detail')
 
 @section('css')
-
+<!-- Datatables -->
+<link rel="stylesheet" href="{{ asset('assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
 @endsection
 
 @section('content')
@@ -13,8 +16,8 @@
               <div class="card-header">
                 <a href="#" @click="addData()" data-target="modal-default" data-toggle="modal" class="btn btn-sm btn-primary pull-right">Create New Transaction Detail</a>
               </div>
-              <div class="card-body p-0">
-                <table id="example2" class="table table-striped">
+              <div class="card-body">
+                <table id="datatable" class="table table-striped table-bordered">
                   <thead>
                     <tr>
                       <th width="30px" class="text-center">No.</th>
@@ -93,6 +96,34 @@
 @endsection
 
 @section('js')
+<!-- Datatables & Plugins -->
+<script src="{{ asset('assets/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/jszip/jszip.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/pdfmake/pdfmake.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/pdfmake/vfs_fonts.js') }}"></script>
+<script src="{{ asset('assets/plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
+<script type="text/javascript">
+    $(function () {
+    $("#datatable").DataTable();
+    // $('#example2').DataTable({
+    //   "paging": true,
+    //   "lengthChange": false,
+    //   "searching": false,
+    //   "ordering": true,
+    //   "info": true,
+    //   "autoWidth": false,
+    //   "responsive": true,
+    // });
+  });
+</script>
+<!-- CRUD Vue js -->
     <script type="text/javascript">
         var controller = new Vue ({
             el: '#controller',
