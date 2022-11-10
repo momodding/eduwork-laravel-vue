8a8ab0604f3372e6201a7e67f6a5011db170faa6
dexalbert <arfiansyah.dede@gmail.com>
2022-11-10 21:30:43 +0800

    eloquent relationship model

--- library/app/Http/Controllers/HomeController.php
+++ library/app/Http/Controllers/HomeController.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Author;
+use App\Models\Book;
+use App\Models\Catalog;
+use App\Models\Member;
+use App\Models\Publisher;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +28,13 @@ class HomeController extends Controller
      */
     public function index()
     {
+        // $members = Member::with('user')->get();
+        // $books = Book::with('publisher')->get();
+        // $publisher = Publisher::with('books')->get();
+        // $catalog = Catalog::with('books')->get();
+        $author = Author::with('books')->get();
+
+        return $author;
         return view('home');
     }
 }
