3ed9d3de32ef377c73b6de601ca3ca941382afc0
Bayu Ramadhon <bayuamdan@gmail.com>
2022-11-22 17:14:56 +0700

    add word & sql

--- bayuramadhon/apotek.sql
+++ bayuramadhon/apotek.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 05, 2022 at 04:37 AM
+-- Generation Time: Nov 22, 2022 at 11:09 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -36,6 +36,22 @@ CREATE TABLE `obat` (
   `tgl_kadaluwarsa` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `obat`
+--
+
+INSERT INTO `obat` (`id`, `id_obat`, `nama_obat`, `pembuat_obat`, `stok_obat`, `tgl_kadaluwarsa`) VALUES
+(1, 0, 'biogesic', 'Ari', 50, '2023-01-01'),
+(2, 0, 'panadol', 'Banu', 50, '2023-01-04'),
+(3, 0, 'neozep', 'Nina', 45, '2023-01-12'),
+(4, 0, 'Mylanta', 'Risma', 60, '2023-02-08'),
+(5, 0, 'Sangobion', 'Dani', 25, '2023-03-01'),
+(6, 0, 'Bodrex', 'Valdi', 100, '2022-02-09'),
+(7, 0, 'Combantrin', 'Reza', 90, '2023-01-28'),
+(8, 0, 'Amoxcilin', 'Vina', 75, '2022-05-04'),
+(9, 0, 'Salonpas', 'Sari', 60, '2024-06-12'),
+(10, 0, 'Mixagrip', 'Agus', 45, '2023-11-08');
+
 -- --------------------------------------------------------
 
 --
@@ -47,6 +63,22 @@ CREATE TABLE `pasien` (
   `nama_pasien` varchar(30) NOT NULL,
   `tanggal_lahir_pasien` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pasien`
+--
+
+INSERT INTO `pasien` (`id_pasien`, `nama_pasien`, `tanggal_lahir_pasien`) VALUES
+(1, 'hilda', '2012-11-08'),
+(2, 'zilong', '2013-11-06'),
+(3, 'vale', '2012-08-14'),
+(4, 'valir', '2013-02-07'),
+(5, 'miya', '2022-03-11'),
+(6, 'layla', '2013-12-20'),
+(7, 'thamuz', '2014-07-09'),
+(8, 'lancelot', '2013-09-12'),
+(9, 'freya', '2022-08-31'),
+(10, 'selena', '2017-10-11');
 
 -- --------------------------------------------------------
 
@@ -81,7 +113,9 @@ ALTER TABLE `pasien`
 -- Indexes for table `transaksi`
 --
 ALTER TABLE `transaksi`
-  ADD PRIMARY KEY (`id_transaksi`);
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `fk_obat` (`id_obat`),
+  ADD KEY `fk_pasien` (`id_pasien`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -91,19 +125,30 @@ ALTER TABLE `transaksi`
 -- AUTO_INCREMENT for table `obat`
 --
 ALTER TABLE `obat`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `pasien`
 --
 ALTER TABLE `pasien`
-  MODIFY `id_pasien` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_pasien` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `transaksi`
 --
 ALTER TABLE `transaksi`
   MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `fk_obat` FOREIGN KEY (`id_obat`) REFERENCES `obat` (`id`),
+  ADD CONSTRAINT `fk_pasien` FOREIGN KEY (`id_pasien`) REFERENCES `pasien` (`id_pasien`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
