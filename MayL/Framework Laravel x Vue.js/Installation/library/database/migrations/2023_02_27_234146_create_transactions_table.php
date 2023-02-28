1aa369cdedde6dc2618e4c9d2d8afc87125f485c
maylius <maylius.riswanda@gmail.com>
2023-02-28 07:59:33 +0700

    make Migration and Model
    membuat migration dan model author, publisher, catalog dan relasi

--- MayL/Framework Laravel x Vue.js/Installation/library/database/migrations/2023_02_27_234146_create_transactions_table.php
+++ MayL/Framework Laravel x Vue.js/Installation/library/database/migrations/2023_02_27_234146_create_transactions_table.php
@@ -11,14 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('transactions', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->unsignedBigInteger('member_id')->nullable();
+            $table->unsignedBigInteger('member_id');
+            $table->date('date_start');
+            $table->date('date_end');
             $table->timestamps();
 
             $table->foreign('member_id')->references('id')->on('members');
@@ -30,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('transactions');
     }
 };
