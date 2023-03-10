3d321b10c8a63d9e6046d7f5a4ba17f723701cf6
maylius <maylius.riswanda@gmail.com>
2023-03-10 21:26:30 +0700

    compact data

--- MayL/Framework Laravel x Vue.js/Installation/library/app/Http/Controllers/CatalogController.php
+++ MayL/Framework Laravel x Vue.js/Installation/library/app/Http/Controllers/CatalogController.php
@@ -14,7 +14,10 @@ class CatalogController extends Controller
      */
     public function index()
     {
-        return view('admin.catalog.index');
+        $catalogs = Catalog::with('catalogs')->get();
+
+        //return $catalogs;
+        return view('admin.catalog.index', compact('catalogs'));
     }
 
     /**
--- MayL/Framework Laravel x Vue.js/Installation/library/resources/views/admin/catalog/index.blade.php
+++ MayL/Framework Laravel x Vue.js/Installation/library/resources/views/admin/catalog/index.blade.php
@@ -3,4 +3,62 @@
 
 @section('content')
     ini adalah halaman catalog
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="card">
+                <div class="card-header">
+                  <h3 class="card-title">Bordered Table</h3>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body">
+                  <table class="table table-bordered">
+                    <thead>
+                      <tr>
+                        <th style="width: 10px">#</th>
+                        <th>Name</th>
+                        <th>Total Book</th>
+                        <th>Created At</th>
+                        <th>Updated At</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($catalogs as $key => $catalog)
+                            <tr>
+                                <td>{{ $key+1 }}</td>
+                                <td>{{ $catalog->name }}</td>
+                                <td>{{ count($catalog->catalogs) }}</td>
+                                <td>{{ date('H:i - d M Y', strtotime($catalog->created_at)) }}</td>
+                                <td>{{ date('H:i - d M Y', strtotime($catalog->updated_at)) }}</td>
+                            </tr>
+                        @endforeach
+                      
+                    </tbody>
+                  </table>
+                </div>
+                <!-- /.card-body -->
+                {{-- <div class="card-footer clearfix">
+                  <ul class="pagination pagination-sm m-0 float-right">
+                    <li class="page-item"><a class="page-link" href="#">&laquo;</a></li>
+                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item"><a class="page-link" href="#">&raquo;</a></li>
+                  </ul> 
+                </div> --}}
+              </div>
+              <!-- /.card -->
+  
+              
+                <!-- /.card-body -->
+              </div>
+              <!-- /.card -->
+            </div>
+          </div>
+          <!-- /.row -->
+        </div><!-- /.container-fluid -->
+      </section>
+      <!-- /.content -->
 @endsection
