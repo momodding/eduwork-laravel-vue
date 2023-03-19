a1f6d929c39e2de3ff3cc375b9042d4734290993
Bayu Ramadhon <bayuamdan@gmail.com>
2023-03-19 16:05:50 +0700

    crud php native

--- bayuramadhon/conndb.php
+++ bayuramadhon/conndb.php
@@ -10,6 +10,18 @@ if(!$conn) {
     die("connection failed: " .  mysqli_connect_error());
 }
 
-echo "berhasil";
-mysqli_close($conn);
+// echo "berhasil";
+// mysqli_close($conn);
+
+$sql = "SELECT * FROM buku";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "Buku :" . $row["isbn"]." ".$row["judul"]." ".$row["tahun"]. "<br>";
+    }
+}else{
+    echo "0 results";
+    }
+$conn->close();
  ?>
