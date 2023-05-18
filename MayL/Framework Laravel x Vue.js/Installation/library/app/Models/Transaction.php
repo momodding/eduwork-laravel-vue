002742f887a573b1e4664f6c9de04faa8e416774
maylius <maylius.riswanda@gmail.com>
2023-05-18 20:16:21 +0700

    tidak bisa save
    transaction.blade\edit

--- MayL/Framework Laravel x Vue.js/Installation/library/app/Models/Transaction.php
+++ MayL/Framework Laravel x Vue.js/Installation/library/app/Models/Transaction.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['member_id', 'status', 'date_start','date_end'];
+
+    public function transactionDetail()
+    {
+        return $this->hasOne('App\Models\TransactionDetail', 'transaction_id');
+    }
 }
