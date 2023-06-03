98a922c442daaa659d0f4b0994fe8bfe7c81209f
Yongki Surya Alkayyis <yongkisurya99@gmail.com>
2023-06-03 14:35:42 +0700

    CRUD without vue js

--- yongki/library/app/Http/Controllers/CatalogController.php
+++ yongki/library/app/Http/Controllers/CatalogController.php
@@ -27,7 +27,7 @@ class CatalogController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.catalog.create');
     }
 
     /**
@@ -38,7 +38,17 @@ class CatalogController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request,[
+            'name'      =>['required'],
+        ]);
+
+       // $catalog = new Catalog;
+       // $catalog->name = $request->name;
+       // $catalog->save();
+
+       Catalog::create($request->all());
+
+        return redirect('catalogs');
     }
 
     /**
@@ -60,7 +70,7 @@ class CatalogController extends Controller
      */
     public function edit(Catalog $catalog)
     {
-        //
+        return view('admin.catalog.edit', compact('catalog'));
     }
 
     /**
@@ -72,7 +82,15 @@ class CatalogController extends Controller
      */
     public function update(Request $request, Catalog $catalog)
     {
-        //
+        $this->validate($request,[
+            'name'      =>['required'],
+        ]);
+
+       
+
+       $catalog->update($request->all());
+
+        return redirect('catalogs');
     }
 
     /**
@@ -83,6 +101,8 @@ class CatalogController extends Controller
      */
     public function destroy(Catalog $catalog)
     {
-        //
+        $catalog->delete();
+
+        return redirect('catalogs');
     }
 }
--- yongki/library/resources/views/admin/catalog/index.blade.php
+++ yongki/library/resources/views/admin/catalog/index.blade.php
@@ -8,7 +8,7 @@
             <div class="col-md-6">
                 <div class="card">
                     <div class="card-header">
-                        <h3 class="card-title">Data Catalog</h3>
+                        <a href="{{ url('catalogs/create') }}" class="btn btn-sm btn-primary pull-right">Create New Catalog</a>
                     </div>
 
                     <div class="card-body"> 
@@ -19,16 +19,26 @@
                                     <th class="text-center">Name</th>
                                     <th class="text-center">Total Books</th>
                                     <th class="text-center">Created At</th>
+                                    <th class="text-center">Action</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                @foreach($catalogs as $key => $catalog)
+                                @foreach($catalogs as $key => $catalog) 
                                 <tr>
                                     <td>{{ $key+1 }}</td>
                                     <td>{{ $catalog->name }}</td>
                                     <td class="text-center">{{ count($catalog->books) }}</td>
                                     <td class="text-center">{{ date('H:i:s - d M Y', strtotime($catalog->created_at)) }}</td>
-                                
+                                    <td class="text-center">
+                                        <a href="{{ url('catalogs/'.$catalog->id.'/edit') }}" class="btn btn-warning btn-sm">Edit</a>
+                                        <form action="{{ url('catalogs', ['id' => $catalog->id]) }}" method="post">
+                                        <input class="btn btn-danger btn-sm" type="submit" value="Delete" onclick="return confirm('Are you sure?')">
+                                        @method('delete')
+                                        @csrf
+                                    </form>
+                                    </td>
+                                    
+
                                 </tr>
                                 @endforeach
                             </tbody>
