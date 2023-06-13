29cf3d969c74b5b9f1818fdadf2c74c33d9ec585
maylius <maylius.riswanda@gmail.com>
2023-06-13 22:16:20 +0700

    revisi tugas p2
    list tabel order by created at desc				<<sudah
    
    delete this.actionUrl						<<sudah, buat variabel baru
    
    input field pakai css class bootstrap styling form input	<<belum
    filter
    
    tambah button samping search reset filter			<<sudah, pakai clear
    
    filter dikasih name per filter					<sudag, disamping

--- MayL/Framework Laravel x Vue.js/Installation/library/resources/views/admin/transaction/detail.blade.php
+++ MayL/Framework Laravel x Vue.js/Installation/library/resources/views/admin/transaction/detail.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
     <!-- Horizontal Form -->
-    <div class="card card-info">
+    <div class="card card-info" id="controller">
         <div class="card-header">
             <h3 class="card-title">Transaction Detail</h3>
         </div>
@@ -24,7 +24,7 @@
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label class="col-sm-2 col-form-label">Buku</label>
+                    <label class="col-sm-2 col-form-label">Judul Buku</label>
                     <div class="col-sm-10">
                             {{ $transactions->title }}
                     </div>
@@ -32,13 +32,21 @@
                 <div class="form-group row">
                     <label class="col-sm-2 col-form-label">Status</label>
                     <div class="col-sm-10">
-                        {{ $transactions->status }}
+                        {{-- {{ $transactions->status }} --}}
+                        @if($transactions->status==0)
+                            <label>Belum dikembalikan</label>
+                        @else
+                            <label>Sudah dikembalikan</label>
+                        @endif
                     </div>
                 </div>
             </div>
             <!-- /.card-body -->
             <div class="card-footer">
-                <button type="submit" class="btn btn-default float-right" href="{{ url('transactions/') }}">Close</button>
+                <button type="submit" class="btn btn-default float-right" action="action"
+                onclick="window.history.go(-1); return false;"
+                type="submit"
+                value="Cancel">Close</button>
             </div>
             <!-- /.card-footer -->
         </form>
@@ -46,5 +54,21 @@
     <!-- /.card -->
 @endsection
 @section('js')
+<script type="text/javascript">
+    var actionUrl = '{{ url('transactions') }}';
 
+    var app = new Vue({
+  el: '#controller',
+  data: {
+    actionUrl,
+    },
+    methods:{
+        back(){
+            console.log("sudah diklik")
+            location.href = this.actionUrl;
+        }
+    }
+})
+
+</script>
 @endsection
