adad548d66beaa43da166107079452c5ea78fb7b
unknown <bayuamdan@gmail.com>
2023-06-25 09:00:52 +0700

    query builder task

--- bayuramadhon/library/app/Http/Controllers/HomeController.php
+++ bayuramadhon/library/app/Http/Controllers/HomeController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Member;
+use Illuminate\Support\Facades\DB;
 use App\Models\Book;
 use App\Models\Publisher;
 use App\Models\Author;
 use App\Models\Catalog;
 use App\Models\Transaction;
 use App\Models\TransactionDetail;
+
 
 class HomeController extends Controller
 {
@@ -64,11 +66,14 @@ class HomeController extends Controller
             ->orderBy('members.id', 'asc')
             ->get();
 
-        //no5
-        // $data5 = Member::select('members.id', 'members.name', 'members.phone_number')
-        //     ->join('transactions', 'transactions.member_id', '=', 'members.id')
-        //     ->groupBy('members.id',  '>', 1)
-        //     ->get();
+        //no 5
+        $data5 = Member::select('members.id', 'members.name', 'members.phone_number')
+            ->whereIn('members.id', function ($query) {
+                $query->select('member_id')
+                    ->from('transactions')
+                    ->groupBy(DB::raw('1'))
+                    ->having(DB::raw('count(member_id)'), '>', 1);
+            })->get();
 
 
         //no 6
@@ -98,9 +103,31 @@ class HomeController extends Controller
             ->get();
 
         // no 10
-        $data10 = Member::select('members.id', 'members.phone_number', 'members.address', 'transactions.date_start', 'transactions.date_end')
+        $data10 = Member::select('members.id', 'members.name', 'members.phone_number', 'members.address', 'transactions.date_start', 'transactions.date_end')
             ->join('transactions', 'transactions.member_id', '=', 'members.id')
             ->where('members.address', 'LIKE', '%bandung%')
+            ->get();
+
+        //no 11
+        $data11 = Member::select('members.id', 'members.name', 'members.phone_number', 'members.address', 'members.gender', 'transactions.date_start', 'transactions.date_end')
+            ->join('transactions', 'transactions.member_id', '=', 'members.id')
+            ->where('members.address', 'LIKE', '%bandung%')
+            ->where('members.gender', 'LIKE', 'f')
+            ->get();
+
+        //no 12
+        $data12 = Member::select('members.id', 'members.name', 'members.phone_number', 'members.address', 'transactions.date_start', 'transactions.date_end', 'books.isbn', 'books.qty')
+            ->join('transactions', 'transactions.member_id', '=', 'members.id')
+            ->join('transaction_details', 'transaction_details.transaction_id', '=', 'transaction_id')
+            ->join('books', 'transaction_details.book_id', '=', 'books.id')
+            ->where('books.isbn', '>', 1)
+            ->get();
+
+        //no 13
+        $data13 = Member::select('members.id', 'members.name', 'members.phone_number', 'members.address', 'transactions.date_start', 'transactions.date_end', 'books.isbn', 'books.qty', 'books.price', DB::raw('SUM(books.qty * books.price) as total'))
+            ->join('transactions', 'transactions.member_id', '=', 'members.id')
+            ->join('transaction_details', 'transaction_details.transaction_id', '=', 'transaction_id')
+            ->join('books', 'transaction_details.book_id', '=', 'books.id')
             ->get();
 
         //no14
@@ -118,19 +145,28 @@ class HomeController extends Controller
             ->join('catalogs', 'books.catalog_id', '=', 'catalogs.id')
             ->get();
 
+        //no 16
+        $data16 = Book::select('author_id', 'authors.name')
+            ->leftjoin('authors', 'books.author_id', 'authors.id')
+            ->get();
 
+        //no17
+        $data17 = Book::select(DB::raw('COUNT(*) as jumlah_pengarang'))
+            ->where('author_id', '4')
+            ->get();
         //no18
         $data18 = Book::select('*')
             ->where('price', '>', 10000)
             ->get();
 
         //no 20
-        // $data20 = Member::select('*')
-        //     ->whereMonth('')
+        $data20 = Member::select('*')
+            ->whereMonth('members.created_at', '6')
+            ->get();
 
 
 
-        return $data10;
+        return $data12;
         return view('home');
     }
 }
