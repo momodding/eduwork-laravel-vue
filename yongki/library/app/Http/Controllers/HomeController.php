bccc9fdcb7d7dfd3af8707044a37b09bd5914df7
Yongki Surya Alkayyis <yongkisurya99@gmail.com>
2023-08-02 16:11:14 +0700

    create graphics

--- yongki/library/app/Http/Controllers/HomeController.php
+++ yongki/library/app/Http/Controllers/HomeController.php
@@ -9,7 +9,18 @@ use App\Models\Author;
 use App\Models\Book;
 use App\Models\Transaction;
 use App\Models\TransactionDetail;
+use App\Models\User;
 use Illuminate\Support\Facades\DB;
+
+// use DB;
+// use App\Buku;
+// use App\Katalog;
+// use App\Anggota;
+// use App\Penerbit;
+// use App\Pengarang;
+// use App\Peminjaman;
+
+use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
@@ -39,124 +50,170 @@ class HomeController extends Controller
 
         //QUERY BUILDER
         //no 1
-        $data = Member::select('*')
-                    ->join('users','users.member_id','=','members.id')
-                    ->get();
+    //     $data = Member::select('*')
+    //                 ->join('users','users.member_id','=','members.id')
+    //                 ->get();
         
-        //no 2
-        $data2 = Member::select('*')
-                    ->leftjoin('users','users.member_id','=','members.id')
-                    ->where('users.id','=',NULL)
-                    ->get();
+    //     //no 2
+    //     $data2 = Member::select('*')
+    //                 ->leftjoin('users','users.member_id','=','members.id')
+    //                 ->where('users.id','=',NULL)
+    //                 ->get();
 
-        //no 3
-        $data3 = Transaction::select('members.id', 'members.name')
-                    ->rightJoin('members', 'members.id', '=','transactions.member_id')
-                    ->where('transactions.member_id', NULL)
-                    ->get();
+    //     //no 3
+    //     $data3 = Transaction::select('members.id', 'members.name')
+    //                 ->rightJoin('members', 'members.id', '=','transactions.member_id')
+    //                 ->where('transactions.member_id', NULL)
+    //                 ->get();
 
-        $data4 = Member::select('members.id', 'members.name', 'members.phone_number')
-                    ->Join('transactions', 'transactions.member_id', '=', 'members.id')
-                    ->orderBy('members.id', 'ASC')
-                    ->get();
+    //     $data4 = Member::select('members.id', 'members.name', 'members.phone_number')
+    //                 ->Join('transactions', 'transactions.member_id', '=', 'members.id')
+    //                 ->orderBy('members.id', 'ASC')
+    //                 ->get();
 
-        $data5 = Member::select('members.id','members.name','members.phone_number')
-                    ->join('transactions','transactions.member_id','=','members.id')
-                    ->where('transactions.id','>','2')
-                    ->orderBy('members.id', 'ASC')
-                    ->get();
+    //     $data5 = Member::select('members.id','members.name','members.phone_number')
+    //                 ->join('transactions','transactions.member_id','=','members.id')
+    //                 ->where('transactions.id','>','2')
+    //                 ->orderBy('members.id', 'ASC')
+    //                 ->get();
 
-        $data6 = Member::select('members.name','members.phone_number','members.address','transactions.date_start','transactions.date_end')
-                    ->join('transactions', 'transactions.member_id','=','members.id')
-                    ->orderBy('members.name', 'ASC')
-                    ->get();
+    //     $data6 = Member::select('members.name','members.phone_number','members.address','transactions.date_start','transactions.date_end')
+    //                 ->join('transactions', 'transactions.member_id','=','members.id')
+    //                 ->orderBy('members.name', 'ASC')
+    //                 ->get();
         
-        $data7 = Member::select('members.name', 'members.phone_number', 'members.address', 'transactions.date_start', 'transactions.date_end')
-                    ->Join('transactions', 'transactions.member_id', '=', 'members.id')->whereMonth('transactions.date_end', '=', '6')
-                    ->orderBy('transactions.date_start', 'ASC')
-                    ->get();
+    //     $data7 = Member::select('members.name', 'members.phone_number', 'members.address', 'transactions.date_start', 'transactions.date_end')
+    //                 ->Join('transactions', 'transactions.member_id', '=', 'members.id')->whereMonth('transactions.date_end', '=', '6')
+    //                 ->orderBy('transactions.date_start', 'ASC')
+    //                 ->get();
 
-        $data8 = Member::select('members.name', 'members.phone_number', 'members.address', 'transactions.date_start', 'transactions.date_end')
-                    ->Join('transactions', 'transactions.member_id', '=', 'members.id')
-                    ->whereMonth('transactions.date_start', '=', '5')
-                    ->orderBy('transactions.date_start', 'ASC')
-                    ->get();
+    //     $data8 = Member::select('members.name', 'members.phone_number', 'members.address', 'transactions.date_start', 'transactions.date_end')
+    //                 ->Join('transactions', 'transactions.member_id', '=', 'members.id')
+    //                 ->whereMonth('transactions.date_start', '=', '5')
+    //                 ->orderBy('transactions.date_start', 'ASC')
+    //                 ->get();
 
-        $data9 = Member::select('members.name', 'members.phone_number', 'members.address', 'transactions.date_start', 'transactions.date_end')
-                    ->Join('transactions', 'transactions.member_id', '=', 'members.id')->where('transactions.date_start','=',6)
-                    ->whereMonth ('transactions.date_end', '=', '6')
-                    ->orderBy('transactions.date_start', 'ASC')
-                    ->get();
+    //     $data9 = Member::select('members.name', 'members.phone_number', 'members.address', 'transactions.date_start', 'transactions.date_end')
+    //                 ->Join('transactions', 'transactions.member_id', '=', 'members.id')->where('transactions.date_start','=',6)
+    //                 ->whereMonth ('transactions.date_end', '=', '6')
+    //                 ->orderBy('transactions.date_start', 'ASC')
+    //                 ->get();
 
-        $data10 = Member::select('members.name', 'members.phone_number', 'members.address', 'transactions.date_start', 'transactions.date_end')
-                     ->Join('transactions', 'transactions.member_id', '=', 'members.id')
-                     ->where('members.address',"%Bandung%")
-                    ->orderBy('transactions.date_start', 'ASC')
-                    ->get();
+    //     $data10 = Member::select('members.name', 'members.phone_number', 'members.address', 'transactions.date_start', 'transactions.date_end')
+    //                  ->Join('transactions', 'transactions.member_id', '=', 'members.id')
+    //                  ->where('members.address',"%Bandung%")
+    //                 ->orderBy('transactions.date_start', 'ASC')
+    //                 ->get();
 
-        $data11 = Member::select('members.name', 'members.phone_number', 'members.address', 'transactions.date_start', 'transactions.date_end')
-                    ->Join('transactions', 'transactions.member_id', '=', 'members.id')->where('members.address',"%Bandung%")
-                    ->where('members.gender','P')->orderBy('transactions.date_start', 'ASC')
-                    ->get();
+    //     $data11 = Member::select('members.name', 'members.phone_number', 'members.address', 'transactions.date_start', 'transactions.date_end')
+    //                 ->Join('transactions', 'transactions.member_id', '=', 'members.id')->where('members.address',"%Bandung%")
+    //                 ->where('members.gender','P')->orderBy('transactions.date_start', 'ASC')
+    //                 ->get();
 
-        $data12 = Member::select('members.name', 'members.phone_number', 'members.address', 'transactions.date_start', 'transactions.date_end', 'transaction_details.qty', 'books.isbn')
-                    ->Join('transactions', 'transactions.member_id', '=', 'members.id')
-                    ->Join('transaction_details', 'transaction_details.transaction_id', '=', 'transactions.id')
-                    ->Join('books','books.id', '=', 'transaction_details.book_id')
-                    ->where('transaction_details.qty','>', 1)
-                    ->orderBy('members.name')
-                    ->get();
+    //     $data12 = Member::select('members.name', 'members.phone_number', 'members.address', 'transactions.date_start', 'transactions.date_end', 'transaction_details.qty', 'books.isbn')
+    //                 ->Join('transactions', 'transactions.member_id', '=', 'members.id')
+    //                 ->Join('transaction_details', 'transaction_details.transaction_id', '=', 'transactions.id')
+    //                 ->Join('books','books.id', '=', 'transaction_details.book_id')
+    //                 ->where('transaction_details.qty','>', 1)
+    //                 ->orderBy('members.name')
+    //                 ->get();
 
-       $data13 = Member::select('members.name', 'members.phone_number', 'members.address', 'transactions.date_start', 'transactions.date_end', 
-                                'transaction_details.qty', 'books.isbn', 'books.price', DB::raw('sum(transaction_details.qty * books.price) as total_price'))
-                    ->Join('transactions', 'transactions.member_id', '=', 'members.id')
-                    ->Join('transaction_details', 'transaction_details.transaction_id', '=', 'transactions.id')
-                    ->Join('books','books.id', '=', 'transaction_details.book_id')
-                    ->groupBy('members.name')
-                    ->orderBy('total_price')
-                    ->get();
+    //    $data13 = Member::select('members.name', 'members.phone_number', 'members.address', 'transactions.date_start', 'transactions.date_end', 
+    //                             'transaction_details.qty', 'books.isbn', 'books.price', DB::raw('sum(transaction_details.qty * books.price) as total_price'))
+    //                 ->Join('transactions', 'transactions.member_id', '=', 'members.id')
+    //                 ->Join('transaction_details', 'transaction_details.transaction_id', '=', 'transactions.id')
+    //                 ->Join('books','books.id', '=', 'transaction_details.book_id')
+    //                 ->groupBy('members.name')
+    //                 ->orderBy('total_price')
+    //                 ->get();
 
-        $data14 = Member::select('members.name', 'members.phone_number', 'members.address', 'transactions.date_start', 'transactions.date_end', 'transaction_details.qty', 'books.isbn', 'books.price', 'authors.name', 'catalogs.name')
-                    ->Join('transactions', 'transactions.member_id', '=', 'members.id')
-                    ->Join('transaction_details', 'transaction_details.transaction_id', '=', 'transactions.id')
-                    ->Join('books','books.id', '=', 'transaction_details.book_id')
-                    ->Join('catalogs', 'catalogs.id', '=', 'books.catalog_id')
-                    ->Join('authors', 'authors.id', '=', 'books.author_id')
-                    ->orderBy('members.name')
-                    ->get();
+    //     $data14 = Member::select('members.name', 'members.phone_number', 'members.address', 'transactions.date_start', 'transactions.date_end', 'transaction_details.qty', 'books.isbn', 'books.price', 'authors.name', 'catalogs.name')
+    //                 ->Join('transactions', 'transactions.member_id', '=', 'members.id')
+    //                 ->Join('transaction_details', 'transaction_details.transaction_id', '=', 'transactions.id')
+    //                 ->Join('books','books.id', '=', 'transaction_details.book_id')
+    //                 ->Join('catalogs', 'catalogs.id', '=', 'books.catalog_id')
+    //                 ->Join('authors', 'authors.id', '=', 'books.author_id')
+    //                 ->orderBy('members.name')
+    //                 ->get();
 
-        $data15 = Catalog::select('catalogs.id', 'catalogs.name', 'books.title')
-                    ->Join('books', 'books.catalog_id', '=', 'catalogs.id')
-                    ->orderBy('catalogs.id')
-                    ->get();
+    //     $data15 = Catalog::select('catalogs.id', 'catalogs.name', 'books.title')
+    //                 ->Join('books', 'books.catalog_id', '=', 'catalogs.id')
+    //                 ->orderBy('catalogs.id')
+    //                 ->get();
 
-        $data16 = Book::select('*', 'publishers.name')
-                    ->LeftJoin('publishers', 'publishers.id', '=', 'books.publisher_id')
-                    ->Union( Book::select('*', 'publishers.name')
-                    ->RightJoin('publishers', 'publishers.id', '=', 'books.publisher_id'))
-                    ->get();
+    //     $data16 = Book::select('*', 'publishers.name')
+    //                 ->LeftJoin('publishers', 'publishers.id', '=', 'books.publisher_id')
+    //                 ->Union( Book::select('*', 'publishers.name')
+    //                 ->RightJoin('publishers', 'publishers.id', '=', 'books.publisher_id'))
+    //                 ->get();
 
-        $data17 = Book::select('authors.id', DB::raw('count(books.id) as total_author'))
-                    ->where('books.author_id', 3)
-                    ->Join('authors', 'authors.id', '=', 'books.author_id')
-                    ->get();
+    //     $data17 = Book::select('authors.id', DB::raw('count(books.id) as total_author'))
+    //                 ->where('books.author_id', 3)
+    //                 ->Join('authors', 'authors.id', '=', 'books.author_id')
+    //                 ->get();
 
-        $data18 = Book::select('*')
-                    ->where('books.price', '>', '10000')
-                    ->get();
+    //     $data18 = Book::select('*')
+    //                 ->where('books.price', '>', '10000')
+    //                 ->get();
 
-        $data19 = Book::select('*')
-                    ->Join('publishers', 'publishers.id', '=', 'books.publisher_id')
-                    ->where('publishers.name', 'like', '%Publisher01%')
-                    ->where('books.qty', '>', '10')
-                    ->get();
+    //     $data19 = Book::select('*')
+    //                 ->Join('publishers', 'publishers.id', '=', 'books.publisher_id')
+    //                 ->where('publishers.name', 'like', '%Publisher01%')
+    //                 ->where('books.qty', '>', '10')
+    //                 ->get();
 
-        $data20 = Member::select('*')
-                    ->whereMonth('members.created_at', '6')
-                    ->groupBy('members.name')
-                    ->get();
+    //     $data20 = Member::select('*')
+    //                 ->whereMonth('members.created_at', '6')
+    //                 ->groupBy('members.name')
+    //                 ->get();
         
-       // return $data17;
-        return view('home');
-    }
+    //    // return $data17;
+    //     //return view('home');
+
+         // ? TOTAL BOX 
+         $total_books = Book::count();
+         $total_publishers = Publisher::count();
+         $total_authors = Author::count();
+         $total_transactions = Transaction::whereMonth('date_start', date('m'))->count();
+ 
+         // ? GRAFIK PUBLISHER/ DONUT
+         $label_donut = Publisher::orderBy('publishers.id', 'asc')
+                            ->join('books', 'books.publisher_id', '=', 'publishers.id')
+                            ->groupBy('publishers.name')
+                            ->pluck('publishers.name');
+ 
+         $data_donut = Book::select(DB::raw("COUNT('publisher_id') as total"))
+                            ->groupBy('publisher_id')
+                            ->orderBy('publisher_id', 'asc')
+                            ->pluck('total');
+
+                            
+         // ? GRAFIK TRANSACTION/ BAR
+         $label_bar = ['Peminjaman', 'Pengembalian'];
+         $data_bar = [];
+ 
+         foreach ( $label_bar as $key => $value ) {
+             $data_bar [$key]['label'] = $label_bar[$key];
+             $data_bar [$key]['backgroundColor'] = $key == 0 ? 'rgba(60, 141, 188, 0.9)' : 'rgba(210, 241, 222, 1)';
+             $data_month = [];
+ 
+             foreach ( range(1,12) as $month ) {
+                 if ( $key == 0 ) {
+                     $data_month[] = Transaction::select(DB::raw("COUNT(*) as total"))->whereMonth('date_start', $month)->first()->total;
+                 } else {
+                     $data_month[] = Transaction::select(DB::raw("COUNT(*) as total"))->whereMonth('date_end', $month)->first()->total;
+                 }
+             }
+ 
+             $data_bar [$key]['data'] = $data_month;
+         }
+ 
+         // ? GRAFIK 
+         $data_pie = Book::select(DB::raw("COUNT('author_id') as total"))->groupBy('author_id')->orderBy('author_id', 'asc')
+                             ->pluck('total');
+         $label_pie = Author::orderBy('authors.id', 'asc')->join('books', 'books.author_id', '=', 'authors.id')
+                             ->groupBy('authors.name')->pluck('authors.name');
+ 
+         return view('home', compact('total_books', 'total_publishers', 'total_authors', 'total_transactions', 'data_donut', 'label_donut', 'data_bar', 'data_pie', 'label_pie'));
+     }
 }
--- yongki/library/resources/views/home.blade.php
+++ yongki/library/resources/views/home.blade.php
@@ -1,6 +1,251 @@
 @extends('layouts.admin')
-@section('header', 'Home')
+@section('header', 'Dashboard')
 
 @section('content')
-Ini adalah halaman Home
+
+    <div class="row">
+
+        <div class="col-lg-3 col-6">
+          <!-- small box -->
+            <div class="small-box bg-info">
+                <div class="inner">
+                    <h3>{{ $total_books }}</h3>
+                    <p>Total Book</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-book"></i>
+                </div>
+                <a href="{{ url('books') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+        <!-- ./col -->
+
+        <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-success">
+                <div class="inner">
+                    <h3>{{ $total_publishers }}</h3>
+                    <p>Total Publisher</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-stats-bars"></i>
+                </div>
+                <a href="{{ url('publishers') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+        <!-- ./col -->
+
+        <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-warning">
+                <div class="inner">
+                    <h3>{{ $total_authors }}</h3>
+                    <p>Total Author</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-person"></i>
+                </div>
+                <a href="{{ url('authors') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+        <!-- ./col -->
+
+        <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-danger">
+                <div class="inner">
+                    <h3>{{ $total_transactions }}</h3>
+                    <p>Total Transaction</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-pie-graph"></i>
+                </div>
+                <a href="{{ url('transactions') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+              
+            <div class="col-lg-6 col-6">
+                <div class="card card-success">
+                    <div class="card-header">
+                        <h3 class="card-title">Grafik Publisher</h3>
+                        <div class="card-tools">
+                            <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                <i class="fas fa-minus"></i>
+                            </button>
+                            <button type="button" class="btn btn-tool" data-card-widget="remove">
+                                <i class="fas fa-times"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="chartjs-size-monitor">
+                            <div class="chartjs-size-monitor-expand">
+                                <div class=""></div>
+                            </div>
+                            <div class="chartjs-size-monitor-shrink">
+                                <div class=""></div>
+                            </div>
+                        </div>
+                        <canvas id="donutChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+                    </div>
+                </div>
+            </div>
+         
+
+        <div class="col-lg-6 col-6">
+            <!-- BAR CHART -->
+            <div class="card card-danger">
+                <div class="card-header">
+                    <h3 class="card-title">Grafik Transaction</h3>
+
+                    <div class="card-tools">
+                        <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                            <i class="fas fa-minus"></i>
+                        </button>
+                        <button type="button" class="btn btn-tool" data-card-widget="remove">
+                            <i class="fas fa-times"></i>
+                        </button>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="chart">
+                        <canvas id="barChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+                    </div>
+                </div>
+              <!-- /.card-body -->
+          </div>
+        </div>
+
+            <div class="col-lg-6 col-6">
+                <div class="card card-warning">
+                    <div class="card-header">
+                        <h3 class="card-title">Grafik Author</h3>
+
+                        <div class="card-tools">
+                            <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                <i class="fas fa-minus"></i>
+                            </button>
+                            <button type="button" class="btn btn-tool" data-card-widget="remove">
+                                <i class="fas fa-times"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="chartjs-size-monitor">
+                            <div class="chartjs-size-monitor-expand">
+                                <div class=""></div>
+                                    </div><div class="chartjs-size-monitor-shrink">
+                                <div class=""></div>
+                        </div></div>
+                        <canvas id="pieChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('js')
+
+<script src="{{ asset('assets/plugins/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/chart.js/Chart.min.js') }}"></script>
+<script type="text/javascript">
+
+    var data_donut = '{!! json_encode($data_donut) !!}';
+    var label_donut = '{!! json_encode($label_donut) !!}';
+    var data_bar = '{!! json_encode($data_bar) !!}';
+    var data_pie = '{!! json_encode($data_pie) !!}';
+    var label_pie = '{!! json_encode($label_pie) !!}';
+
+  $(function () {
+    /* ChartJS
+     * -------
+     * Here we will create a few charts using ChartJS
+     */
+
+    
+    
+    //--------------
+    // ! Donut Chart -
+    //--------------
+
+    // Get context with jQuery - using jQuery's .get() method.
+    var donutChartCanvas = $('#donutChart').get(0).getContext('2d')
+    var donutData        = {
+      labels: JSON.parse(label_donut),
+      datasets: [
+        {
+          data: JSON.parse(data_donut),
+          backgroundColor : [ '#228B22', '#ADFF2F', '#00FA9A', '#FF00FF', '#BA55D3', '#7B68EE', '#4169E1', '#0000FF',
+                              '#000080', '#8A2BE2'],
+        }
+      ]
+    }
+    var donutOptions     = {
+      maintainAspectRatio : false,
+      responsive : true,
+    }
+    //Create pie or douhnut chart
+    // You can switch between pie and douhnut using the method below.
+    new Chart(donutChartCanvas, {
+      type: 'doughnut',
+      data: donutData,
+      options: donutOptions
+    });
+
+  
+    //-------------
+    // ! BAR CHART -
+    //-------------
+
+    var areaChartData = {
+      labels  : ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'Agustus', 'September', 'October', 'November', 'Desember'],
+      datasets: JSON.parse(data_bar)
+    }
+
+
+    var barChartCanvas = $('#barChart').get(0).getContext('2d')
+    var barChartData = $.extend(true, {}, areaChartData)
+
+    var barChartOptions = {
+      responsive              : true,
+      maintainAspectRatio     : false,
+      datasetFill             : false
+    }
+
+    new Chart(barChartCanvas, {
+      type: 'bar',
+      data: barChartData,
+      options: barChartOptions
+    })
+
+
+   //-------------
+    // ! PIE CHART -
+    //-------------
+    // Get context with jQuery - using jQuery's .get() method.
+    var pieChartCanvas = $('#pieChart').get(0).getContext('2d')
+    var pieData        = {
+      labels: JSON.parse(label_pie),
+      datasets: [
+        {
+          data: JSON.parse(data_pie),
+          backgroundColor : [ '#581845', '#900C3F', '#4A0404', '#00c0ef', '#3c8dbc', '#F0E68C', '#0F52BA', '#800020', '#6F4E37',
+                              '#708090', '#8A2BE2', '#00008B', '#D2691E', '#FF1493', '#228B22', '	#FFD700', '	#ADFF2F'],
+        }
+      ]
+    }
+    var pieOptions     = {
+      maintainAspectRatio : false,
+      responsive : true,
+    }
+    //Create pie or douhnut chart
+    // You can switch between pie and douhnut using the method below.
+    new Chart(pieChartCanvas, {
+      type: 'pie',
+      data: pieData,
+      options: pieOptions
+    })
+  })
+</script>
 @endsection
